dadf62c65ac46cdfa8c91085ee304b729f8cbc23
shehara <shehararodrigo7@gmail.com>
2025-03-31 11:31:27 +1300

    Initial Print Commands

--- Practicetask.py
+++ Practicetask.py
@@ -6,4 +6,9 @@ activities = {
     "2" : ("Science Experiments Lab", 3,"moderate",10),
     "3" : ("Sports Leadership Training",4,"challenging",12)
 }
-print(activities)
+print("\nChoose an activity")
+print("1. Music Jam Session (2 hours, easy, $5 fee)")
+print("2. Science Experiments Lab (3 hours, moderate, $10 fee)")
+print("3. Sports Leadership Training (4 hours, challenging, $12 fee)")
+
+activity_choice = input ("Enter the number of your chosen activity:  ")
